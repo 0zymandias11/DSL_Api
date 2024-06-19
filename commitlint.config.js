f75ac7173d413291e6f19066f0f881f6ca31aabf
0zymandias11 <priyankarroy1711@gmail.com>
2024-06-19 14:48:40 +0530

    build: dsl login api with user creation, authentication and rule parsing abilities

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,24 @@
+// commitlint.config.js
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [
-        2,
-        'always',
-        [
-          'build',
-          'chore',
-          'ci',
-          'docs',
-          'feat',
-          'fix',
-          'perf',
-          'refactor',
-          'revert',
-          'style',
-          'test'
-        ]
-      ],
-      'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']]
-    }
-  };
-  
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']]
+  }
+};
